b074927b6e15eca27b88991ccf43b95008564f0b
lifuqiang <195135955@qq.com>
2014-06-17 19:40:55 +0800

    create xcode project

---
+++ CYFastImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CYFastImage
+//
+//  Created by jason on 14-6-17.
+//  Copyright (c) 2014年 chenyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
