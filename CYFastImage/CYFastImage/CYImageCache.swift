b8481935affcf29e92684ce8293ec97a63ecef6d
lifuqiang <195135955@qq.com>
2014-06-21 01:07:02 +0800

    add cache

---
+++ CYFastImage/CYFastImage/CYImageCache.swift
@@ -0,0 +1,70 @@
+//
+//  CYImageCache.swift
+//  CYFastImage
+//
+//  Created by jason on 14-6-19.
+//  Copyright (c) 2014 chenyang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CYFastImage{
+    typealias ImageCacheCallback = (image: UIImage!, url: String!) -> Void
+    
+    class CYImageCache: NSObject {
+        var cacheQueue: dispatch_queue_t!
+        var defaultCachePath: String
+        var fileManager: NSFileManager
+        
+        init() {
+            cacheQueue = dispatch_queue_create("cyfastimage_cache_queue", nil)
+            var paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+            defaultCachePath = paths[0] as NSString
+            defaultCachePath = defaultCachePath.stringByAppendingPathComponent("com.chenyang.imagecache");
+            
+            fileManager = NSFileManager()
+            
+            super.init()
+        }
+        
+        // MARK: public
+        func getImage(url: String!, callback:ImageCacheCallback!) {
+            if !url || !callback {
+                return
+            }
+            
+            var cachepath = getcachePath(url)
+            
+            DEBUG_LOG(cachepath)
+            
+            var image = UIImage(contentsOfFile: cachepath)
+            callback(image: image, url: url)
+        }
+        
+        func saveImage(url: String!, image: UIImage!) {
+            if !url {
+                return
+            }
+            
+            if image {
+                dispatch_async(cacheQueue){
+                    if !self.fileManager.fileExistsAtPath(self.defaultCachePath) {
+                        self.fileManager.createDirectoryAtPath(self.defaultCachePath, attributes: nil)
+                    }
+                    
+                    var cachePath = self.getcachePath(url)
+                    self.fileManager.createFileAtPath(cachePath, contents: UIImagePNGRepresentation(image), attributes: nil)
+                }
+            }
+        }
+        
+        // MARK: helper
+        func getcachePath(url: String!) -> String! {
+            var hashValue = url?.hashValue
+            var key = hashValue.description
+            
+            return defaultCachePath.stringByAppendingPathComponent(key);
+        }
+    }
+}
